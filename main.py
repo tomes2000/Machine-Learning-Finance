1bdee9e3967fd422997c336c67d25ca1e3613dac
AAVASH DEVKOTA <aavash2000@gmail.com>
2024-06-08 19:43:29 +0100

    adding in SMA and EMA functions

--- main.py
+++ main.py
@@ -5,4 +5,17 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import yfinance as yf
 
-openai.API_KEY = open('API_KEY', 'r').read()
+openai.api_key = open('API_KEY', 'r').read()
+
+def get_stock_price(ticker):
+    return str(yf.Ticker(ticker).history(period='1y').iloc[-1].Close)
+
+def calculate_SMA(ticker, window):
+    data = yf.Ticker(ticker).history(period='1y').Close
+    return str(data.rolling(window=window).mean().iloc[-1])
+
+def calculate_EMA(ticker, window):
+    data = yf.Ticker(ticker).history(period='1y').Close
+    return str(data.ewm(span=window, adjust=False).mean().iloc[-1])
+
+
